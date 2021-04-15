45a450d9a0e5f200d4891107584d6cc0ae086c62
LiamMcFadden <liamm18@vt.edu>
2021-04-15 11:46:33 -0400

    switch

--- app/src/main/java/com/example/a3714_pairprogramming3/MainActivity.java
+++ app/src/main/java/com/example/a3714_pairprogramming3/MainActivity.java
@@ -2,13 +2,67 @@ package com.example.a3714_pairprogramming3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.GestureDetector;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    static final int REQUEST_IMAGE = 1;
+    TouchListener touchListener;
+    GestureDetector gestureDetector;
+    ImageView image;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        touchListener = new TouchListener(this);
+        image = (ImageView) findViewById(R.id.image);
+        image.setOnTouchListener(new OnSwipeTouchListener(this) {
+            @Override
+            public void onSwipeDown() {
+                Toast.makeText(MainActivity.this, "Down", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onSwipeLeft() {
+                Toast.makeText(MainActivity.this, "Left", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onSwipeUp() {
+                Toast.makeText(MainActivity.this, "Up", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onSwipeRight() {
+                Toast.makeText(MainActivity.this, "Right", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    public void onLongPress() {
+        Intent takePicIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(takePicIntent.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(takePicIntent, REQUEST_IMAGE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap thumbnail = (Bitmap) extras.get("data");
+            image.setImageBitmap(thumbnail);
+        }
     }
 }
